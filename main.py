34f0dccc2daf3b260c47a0d82549449999bd39aa
efnan-merdan <efnan.merdan@stu.ssst.edu.ba>
2023-05-08 18:58:49 +0200

    Project complete

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ parser.add_argument('-o', '--save_prefix', type=str, default='saved', help='will
 option = parser.parse_args() #Convert argument strings to objects and assign them as attributes of the namespace. Return the populated namespace.
 
 colorizer_eccv16 = eccv16(pretrained=True).eval()
+colorizer_mefnan23 = mefnan23(pretrained=True).eval()
 
 # What is cude
 # torch.cuda is used to set up and run CUDA operations
@@ -16,6 +17,7 @@ colorizer_eccv16 = eccv16(pretrained=True).eval()
 
 if(option.use_gpu):
     colorizer_eccv16.cuda()
+    colorizer_mefnan23.cuda()
 
 img = load_img(option.img_path)
 (tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256,256))
@@ -25,8 +27,11 @@ if (option.use_gpu):
 
 img_bw = postprocess_tens(tens_l_orig, torch.cat((0 * tens_l_orig, 0 * tens_l_orig), dim=1))
 out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs).cpu())
+out_img_mefnan23 = postprocess_tens(tens_l_orig, colorizer_mefnan23(tens_l_rs).cpu())
 
-plt.imsave('%s_eccv16.png'%option.save_prefix, out_img_eccv16)
+
+plt.imsave('%s_eccv16.jpeg'%option.save_prefix, out_img_eccv16)
+plt.imsave('%s_mefnan23.jpeg'%option.save_prefix, out_img_mefnan23)
 
 plt.figure(figsize=(12,8))
 plt.subplot(2,2,1)
@@ -34,16 +39,23 @@ plt.imshow(img)
 plt.title('Original')
 plt.axis('off')
 
+
 plt.subplot(2,2,2)
 plt.imshow(img_bw)
 plt.title('Input')
 plt.axis('off')
+
 
 plt.subplot(2,2,3)
 plt.imshow(out_img_eccv16)
 plt.title('ECCV16 Method')
 plt.axis('off')
 
+plt.subplot(2,2,4)
+plt.imshow(out_img_mefnan23)
+plt.title('MEFNAN23 Method')
+plt.axis('off')
+plt.show()
 
 
 
--- methods/__init__.py
+++ methods/__init__.py
@@ -1,4 +1,5 @@
 
 from methods.default_color import *
 from methods.eccv16_method import *
+from methods.mefnan23 import *
 from methods.utilization import *
